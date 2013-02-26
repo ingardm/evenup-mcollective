8326de5fa9d0f3b4892ff8eff392cdf9069bd3c7
Justin Lambert <jlambert@eml.cc>
2013-02-26 11:37:52 -0700

    fix subscribe on service['mcollective']

--- manifests/server.pp
+++ manifests/server.pp
@@ -46,7 +46,7 @@ class mcollective::server($enabled = true) {
       ensure     => $running,
       enable     => $enabled,
       require    => Package['mcollective'],
-      subscribe  => File['/etc/mcollective/server.cfg.erb'];
+      subscribe  => File['/etc/mcollective/server.cfg'];
   }
 
   package {
